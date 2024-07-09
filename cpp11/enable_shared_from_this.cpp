96d2ee81737ddbd4e2f828c9ac7631615cdb8abf
RyanDDDDDD <yainwrd@gmail.com>
2024-07-09 21:30:24 +1000

    enable_shared_from_this update

--- cpp11/enable_shared_from_this.cpp
+++ cpp11/enable_shared_from_this.cpp
@@ -1,31 +1,50 @@
 #include <iostream>
 #include <memory>
-#include <vector>
+#include <cassert>
 
-class Widget;
+using namespace std;
 
-std::vector<std::shared_ptr<Widget>> vec;
-
-class Widget : public std::enable_shared_from_this<Widget> {
+class Y: public enable_shared_from_this<Y>
+{
 public:
-    Widget () {
-        std::cout << "construct" << std::endl;
+
+    shared_ptr<Y> f()
+    {
+        // 1.
+        // this will create a shared_ptr with new control block, to manage resource
+        // which means, there will exist multiple shared ptr using different control block which 
+        // reference the same memory resource 
+        // 2. 
+        // on the other hand, it is not safe to return a raw this ptr to user when we use a raw pointer
+        // to reference resource, "this" could be released when the object goes out of the scope
+        // return std::shared_ptr<Y>(this);
+        
+        return shared_from_this();
     }
 
-    void process () {
-        vec.emplace_back (shared_from_this ());
-        // vec.emplace_back (this);
+    void print() {
+        std::cout << "hello world" << std::endl;
     }
 
-    ~Widget () {
-        std::cout << "deconsturct" << std::endl;
-    }
 };
 
-int main () {
+int main()
+{
+    shared_ptr<Y> p(new Y);
+    shared_ptr<Y> q = p->f();
 
-    std::shared_ptr<Widget> ptr = std::make_shared<Widget> ();
+    p->print();
+    q->print();
 
-    ptr->process ();
-    ptr->process ();
+    std::cout << p.use_count() << std::endl;
+    std::cout << q.use_count() << std::endl;
+
+    assert(p == q);
+    assert(!(p < q || q < p)); // p and q must share ownership
+
+
+    std::cout << "end of program" << std::endl;
 }
+
+
+
